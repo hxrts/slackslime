64bfd19c8a2eeca75a1cb9f902ef917a2563ab45
Dan Taeyoung <provolot@gmail.com>
2017-03-24 19:02:58 -0400

    initial refactoring to Wormhole works

---
+++ Wormhole.js
@@ -0,0 +1,298 @@
+var slackAPI = require('slackbotapi');
+var _ = require('lodash');
+var fs = require('fs');
+var request = require('request');
+
+class Wormhole {
+
+  constructor(config) {
+    this.config = config;
+    this.slacks = new Array();
+    this.connectedTeams = 0;
+  }
+
+  run() {
+    var context = this; // using 'context' because some other methods use self and it gets confusing
+
+    //----------------
+    // main event loop
+    //----------------
+    
+    context.config.portals.forEach(function(portal, i) {
+
+        //-------------------
+        // manage connections
+        //-------------------
+
+        // initialize
+        context.slacks[i] = new slackAPI({
+            'token': portal.token,
+            'logging': false,  // for output debug
+            'autoReconnect': true
+        });
+
+        // session info
+        context.slacks[i].on('hello', function(data) {
+
+            var helloInstance = this.slackData.team.name + ' / ' + portal.channelName;
+
+            console.log('\n' + _.repeat(i, helloInstance.length) + '\n' + helloInstance + '\n' + _.repeat(i, helloInstance.length) + '\n');
+
+            if(this.getChannel(portal.channelName)) {
+                this.channelId = this.getChannel(portal.channelName).id;
+                this.channelType = 'public';
+                console.log('connecting to public channel ' + this.channelId + '\n');
+            } else {
+                this.channelId = this.getGroup(portal.channelName).id;
+                this.channelType = 'private';
+                console.log('connecting to private channel ' + this.channelId + '\n');
+            }
+            context.connectedTeams++;
+        });
+
+        // close connection
+        context.slacks[i].on('close', function(data) {
+            context.connectedTeams--;
+        });
+
+
+        //---------------------
+        // typing notifications
+        //---------------------
+
+        context.slacks[i].on('user_typing', function(data) {
+
+            var self = this;
+
+            context.slacks.forEach(function(slack) {
+
+                if(slack.token !== self.token) {
+
+                    if(self.channelType === 'public') {
+
+                        var channelMatch = slack.slackData.channels.filter(function (channelRemote) {
+                            return channelRemote.name === portal.channelName;
+                        })[0];
+
+                    } else {
+
+                        var channelMatch = slack.slackData.groups.filter(function (channelRemote) {
+                            return channelRemote.name === portal.channelName;
+                        })[0];
+
+                    }
+
+                    slack.sendTyping(channelMatch.id);
+                }
+            })
+        });
+
+
+        //--------------
+        // message logic
+        //--------------
+
+        context.slacks[i].on('message', function(data) {
+
+            // handle file_share message subtypes instead of file_shared events
+            // Slack's API doesn't send much info with file_shared anymore
+            if(data.subtype === 'file_share') {
+                onFileShare(this, data);
+                return;
+            }
+
+            var self = this;
+            var user = self.getUser(data.user);
+            var teamName = self.slackData.team.name;
+
+            if(self.channelType === 'public') {
+                var channel = self.getChannel(data.channel);
+            } else {
+                var channel = self.getGroup(data.channel);
+            }
+
+            if(typeof data.text === 'undefined' || data.subtype === 'bot_message' || !channel || channel.name !== portal.channelName) {
+                return;
+            }
+
+            // parse user names
+            if(user) {
+                data.username = user.name;
+                data.iconUrl = user.profile.image_48;
+            }
+
+            // look for mentions and convert user handles to plaintext
+            var re = RegExp("((<@)[^\>]+)>", 'g');
+
+            if(re.test(data.text)) {
+                data.text = data.text.replace(re, function getUserName(userString) {
+                    return '@' + self.getUser(userString.substring(2, userString.length - 1)).name;
+                });
+            }
+
+            // messaging logic
+            if(data.text.charAt(0) === '!') {  // bot / channel commands should go here
+
+                // split command and its arguments into an array
+                var command = data.text.substring(1).split(' ');
+
+                switch(command[0].toLowerCase()) {
+                    case 'list':
+                        // send a list of all users on every chat and send via DM/PM
+                        var message = '', awayCount = 0, activeCount = 0, userCount = 0;
+
+                        context.slacks.forEach(function(s) {
+                            s.reqAPI('channels.info', {channel: s.channelId}, function(d) {
+                                if(d.ok) {
+                                    d.channel.members.forEach(function(user) {
+                                        if(s.getUser(user)) {
+                                            var status = ':question: ';
+                                            userCount++;
+
+                                            if(s.getUser(user).presence === 'active') {
+                                                status = ':coffee: ';
+                                                activeCount++;
+                                            }
+
+                                            if(s.getUser(user).presence === 'away') {
+                                                status = ':zzz: ';
+                                                awayCount++;
+                                            }
+
+                                            message += status + ' `' + (s.slackData.team.name + _.repeat(' ', 16)).substring(0, 16) + '` @' + s.getUser(user).name + '\n';
+                                        }
+                                    })
+                                }
+                            });
+                        });
+
+                        setTimeout(function() {
+                            self.sendPM(
+                                data.user,
+                                message + '––––––––––––––––––––––––\n' +
+                                '*Active:* ' + activeCount + '     ' +
+                                '*Total:* '  + userCount   + '     ' +
+                                '*Teams:* '  + context.connectedTeams
+                                );
+                        }, 2000); // 2 second default wait
+                        break;
+                }
+            }
+
+            else if(!data.subtype) { // send normal user message to other team(s)
+
+                var message = {
+                    channel: '#' + portal.channelName,
+                    text: data.text,
+                    username: data.username + ' @ ' + teamName,
+                    icon_url: data.iconUrl,
+                    unfurl_links: true,
+                    unfurl_media: true
+                };
+
+                context.slacks.forEach(function(slack) {
+                    if(slack.token !== self.token) {
+                        slack.reqAPI('chat.postMessage', message);
+                    }
+                })
+            }
+        });
+
+
+        //--------------
+        // file handling
+        //--------------
+
+        // download handler
+        var downloadSlackFile = function(options, callback) {
+            var stream = request({
+                url: options.url,
+                headers: {
+                  'Authorization': 'Bearer ' + options.token
+                }
+            }).pipe(fs.createWriteStream(options.directory + options.filename));
+            stream.on('finish', callback);
+        }
+
+        // upload handler
+        var uploadSlackFile = function(upload_options, callback) {
+            // this method is here because slackbotapi library can't seem to handle file uploads
+            request.post({
+                url: 'https://slack.com/api/files.upload',
+                formData: upload_options,
+            }, function (err, response) {
+                callback(err, response);
+            });
+        }
+
+
+        // send shared file to other team's public shared store
+        var onFileShare = function(self, data) {
+            // shared files in public channels already have a public URL (!)
+            // todo: ask if the file should be shared
+            var user = self.getUser(data.file.user);
+            var teamName = self.slackData.team.name;
+
+            if(self.channelType === 'public') {
+                var channel = self.getChannel(data.file.channels.splice(-1)[0]);  // https://api.slack.com/types/file
+            } else {
+                var channel = self.getGroup(data.file.channels.splice(-1)[0]);
+            }
+
+            if(channel.name !== portal.channelName) {
+                return;
+            }
+
+            if(user.is_bot) {
+                return;  //otherwise we risk an infinite loop
+            }
+
+            if(user) {  // unfortunately image uploads don't support as-user avatar changes
+                data.username = user.name;
+                data.iconUrl = user.profile.image_48;
+            }
+
+            var download_options = {
+                url: data.file.url_private,
+                directory: context.slackslime.tmpDir,
+                filename: data.file.name,
+                token: self.token
+            };
+
+            var downloaded_file_path = downloadSlackFile(download_options, function() {
+
+                var initial_comment = 'From ' + data.username + ' @ ' +  teamName;
+                if(data.file.comments_count > 0) {
+                  initial_comment += ":\n \"" + data.file.initial_comment.comment + "\"";
+                }
+
+                var upload_options = {
+                    channels: '#' + portal.channelName,
+                    filename: data.file.name,
+                    title: data.username + ' @ ' + teamName + ' posted: ' + data.file.name,
+                    filetype: 'auto',
+                    initial_comment: initial_comment,
+                    file: fs.createReadStream(download_options.directory + download_options.filename)
+                };
+
+                context.slacks.forEach(function(slack) {
+                    if(slack.token !== self.token) {  // so we don't send it to ourselves
+
+                        var this_upload_options = upload_options;
+                        this_upload_options['token'] = slack.token;
+
+                        uploadSlackFile(this_upload_options, function(err, response) {
+                        });
+                    }
+                })
+
+            });  // downloaded_file_path
+
+        }  // slacks[i].on('message', ...)
+
+    });  // context.slackslime.tokens.forEach(function(token, i), ...)
+
+  }
+}
+
+module.exports = Wormhole;
--- slackslime.js
+++ slackslime.js
@@ -7,11 +7,11 @@
 //--------
 
 var _ = require('lodash');
-var slackAPI = require('slackbotapi');
 var tmp = require('tmp');
 var fs = require('fs');
-var request = require('request');
+var argv = require('yargs').argv;
 
+var Wormhole = require('./Wormhole');
 
 //------
 // setup
@@ -19,294 +19,44 @@ var request = require('request');
 
 // create temporary directory for file uploads (obv. this is insecure, so please don't share sensitive info)
 var tmpobj = tmp.dirSync({ template: '/tmp/tmp-slackslime-XXXXXXXXX' });
+var tmpDir = tmpobj.name;
 
+var wormholes = [];
+var slackslime_settings;
 
-// command line args
-var slackslime = {
-    channelName: process.argv[2],
-    tokens: process.argv.slice(3),
-    connectedTeams: 0
-};
 
+if(!('configfile' in argv)) {
+  // legacy format: nodejs slackslime.js [channel name] [RTM API token 1] [RTM API token 2] [RTM API token 3] [more tokens]
+  // command line args
+  var channelName = process.argv[2];
+  var tokens = process.argv.slice(3);
 
-// create slack connection store
-var slacks = new Array();
+  var wormhole_config = {};
 
+  wormhole_config.description = "command-line portal for " + channelName;
 
-//----------------
-// main event loop
-//----------------
+  wormhole_config.portals = [];
+  tokens.forEach(function(t) {
+    wormhole_config.portals.push({ 'token': t, 'channelName': channelName })
+  });
 
-slackslime.tokens.forEach(function(token, i) {
+  wormhole_config.tmpDir = tmpDir;
 
-    //-------------------
-    // manage connections
-    //-------------------
+  wormholes.push(new Wormhole(wormhole_config));
 
-    // initialize
-    slacks[i] = new slackAPI({
-        'token': token,
-        'logging': false,  // for output debug
-        'autoReconnect': true
-    });
+} else {
+  var config = JSON.parse(fs.readFileSync(argv.configfile, 'utf8'))
 
-    // session info
-    slacks[i].on('hello', function(data) {
+  _.forEach(config.wormholes, function(name, whconfig) {
 
-        var helloInstance = this.slackData.team.name + ' / ' + slackslime.channelName;
+    
+  });
 
-        console.log('\n' + _.repeat(i, helloInstance.length) + '\n' + helloInstance + '\n' + _.repeat(i, helloInstance.length) + '\n');
 
-        if(this.getChannel(slackslime.channelName)) {
-            this.channelId = this.getChannel(slackslime.channelName).id;
-            this.channelType = 'public';
-            console.log('connecting to public channel ' + this.channelId + '\n');
-        } else {
-            this.channelId = this.getGroup(slackslime.channelName).id;
-            this.channelType = 'private';
-            console.log('connecting to private channel ' + this.channelId + '\n');
-        }
-        slackslime.connectedTeams++;
-    });
 
-    // close connection
-    slacks[i].on('close', function(data) {
-        slackslime.connectedTeams--;
-    });
+} 
 
 
-    //---------------------
-    // typing notifications
-    //---------------------
-
-    slacks[i].on('user_typing', function(data) {
-
-        var self = this;
-
-        slacks.forEach(function(slack) {
-
-            if(slack.token !== self.token) {
-
-                if(self.channelType === 'public') {
-
-                    var channelMatch = slack.slackData.channels.filter(function (channelRemote) {
-                        return channelRemote.name === slackslime.channelName;
-                    })[0];
-
-                } else {
-
-                    var channelMatch = slack.slackData.groups.filter(function (channelRemote) {
-                        return channelRemote.name === slackslime.channelName;
-                    })[0];
-
-                }
-
-                slack.sendTyping(channelMatch.id);
-            }
-        })
-    });
-
-
-    //--------------
-    // message logic
-    //--------------
-
-    slacks[i].on('message', function(data) {
-
-        // handle file_share message subtypes instead of file_shared events
-        // Slack's API doesn't send much info with file_shared anymore
-        if(data.subtype === 'file_share') {
-            onFileShare(this, data);
-            return;
-        }
-
-        var self = this;
-        var user = self.getUser(data.user);
-        var teamName = self.slackData.team.name;
-
-        if(self.channelType === 'public') {
-            var channel = self.getChannel(data.channel);
-        } else {
-            var channel = self.getGroup(data.channel);
-        }
-
-        if(typeof data.text === 'undefined' || data.subtype === 'bot_message' || !channel || channel.name !== slackslime.channelName) {
-            return;
-        }
-
-        // parse user names
-        if(user) {
-            data.username = user.name;
-            data.iconUrl = user.profile.image_48;
-        }
-
-        // look for mentions and convert user handles to plaintext
-        var re = RegExp("((<@)[^\>]+)>", 'g');
-
-        if(re.test(data.text)) {
-            data.text = data.text.replace(re, function getUserName(userString) {
-                return '@' + self.getUser(userString.substring(2, userString.length - 1)).name;
-            });
-        }
-
-        // messaging logic
-        if(data.text.charAt(0) === '!') {  // bot / channel commands should go here
-
-            // split command and its arguments into an array
-            var command = data.text.substring(1).split(' ');
-
-            switch(command[0].toLowerCase()) {
-                case 'list':
-                    // send a list of all users on every chat and send via DM/PM
-                    var message = '', awayCount = 0, activeCount = 0, userCount = 0;
-
-                    slacks.forEach(function(s) {
-                        s.reqAPI('channels.info', {channel: s.channelId}, function(d) {
-                            if(d.ok) {
-                                d.channel.members.forEach(function(user) {
-                                    if(s.getUser(user)) {
-                                        var status = ':question: ';
-                                        userCount++;
-
-                                        if(s.getUser(user).presence === 'active') {
-                                            status = ':coffee: ';
-                                            activeCount++;
-                                        }
-
-                                        if(s.getUser(user).presence === 'away') {
-                                            status = ':zzz: ';
-                                            awayCount++;
-                                        }
-
-                                        message += status + ' `' + (s.slackData.team.name + _.repeat(' ', 16)).substring(0, 16) + '` @' + s.getUser(user).name + '\n';
-                                    }
-                                })
-                            }
-                        });
-                    });
-
-                    setTimeout(function() {
-                        self.sendPM(
-                            data.user,
-                            message + '––––––––––––––––––––––––\n' +
-                            '*Active:* ' + activeCount + '     ' +
-                            '*Total:* '  + userCount   + '     ' +
-                            '*Teams:* '  + slackslime.connectedTeams
-                            );
-                    }, 2000); // 2 second default wait
-                    break;
-            }
-        }
-
-        else if(!data.subtype) { // send normal user message to other team(s)
-
-            var message = {
-                channel: '#' + slackslime.channelName,
-                text: data.text,
-                username: data.username + ' @ ' + teamName,
-                icon_url: data.iconUrl,
-                unfurl_links: true,
-                unfurl_media: true
-            };
-
-            slacks.forEach(function(slack) {
-                if(slack.token !== self.token) {
-                    slack.reqAPI('chat.postMessage', message);
-                }
-            })
-        }
-    });
-
-
-    //--------------
-    // file handling
-    //--------------
-
-    // download handler
-    var downloadSlackFile = function(options, callback) {
-        var stream = request({
-            url: options.url,
-            headers: {
-              'Authorization': 'Bearer ' + options.token
-            }
-        }).pipe(fs.createWriteStream(options.directory + options.filename));
-        stream.on('finish', callback);
-    }
-
-    // upload handler
-    var uploadSlackFile = function(upload_options, callback) {
-        // this method is here because slackbotapi library can't seem to handle file uploads
-        request.post({
-            url: 'https://slack.com/api/files.upload',
-            formData: upload_options,
-        }, function (err, response) {
-            callback(err, response);
-        });
-    }
-
-
-    // send shared file to other team's public shared store
-    var onFileShare = function(self, data) {
-        // shared files in public channels already have a public URL (!)
-        // todo: ask if the file should be shared
-        var user = self.getUser(data.file.user);
-        var teamName = self.slackData.team.name;
-
-        if(self.channelType === 'public') {
-            var channel = self.getChannel(data.file.channels.splice(-1)[0]);  // https://api.slack.com/types/file
-        } else {
-            var channel = self.getGroup(data.file.channels.splice(-1)[0]);
-        }
-
-        if(channel.name !== slackslime.channelName) {
-            return;
-        }
-
-        if(user.is_bot) {
-            return;  //otherwise we risk an infinite loop
-        }
-
-        if(user) {  // unfortunately image uploads don't support as-user avatar changes
-            data.username = user.name;
-            data.iconUrl = user.profile.image_48;
-        }
-
-        var download_options = {
-            url: data.file.url_private,
-            directory: tmpobj.name,
-            filename: data.file.name,
-            token: self.token
-        };
-
-        var downloaded_file_path = downloadSlackFile(download_options, function() {
-
-            var initial_comment = 'From ' + data.username + ' @ ' +  teamName;
-            if(data.file.comments_count > 0) {
-              initial_comment += ":\n \"" + data.file.initial_comment.comment + "\"";
-            }
-
-            var upload_options = {
-                channels: '#' + slackslime.channelName,
-                filename: data.file.name,
-                title: data.username + ' @ ' + teamName + ' posted: ' + data.file.name,
-                filetype: 'auto',
-                initial_comment: initial_comment,
-                file: fs.createReadStream(download_options.directory + download_options.filename)
-            };
-
-            slacks.forEach(function(slack) {
-                if(slack.token !== self.token) {  // so we don't send it to ourselves
-
-                    var this_upload_options = upload_options;
-                    this_upload_options['token'] = slack.token;
-
-                    uploadSlackFile(this_upload_options, function(err, response) {
-                    });
-                }
-            })
-
-        });  // downloaded_file_path
-
-    }  // slacks[i].on('message', ...)
-
-});  // slackslime.tokens.forEach(function(token, i), ...)
+wormholes.forEach(function(wh) {
+  wh.run();
+});
